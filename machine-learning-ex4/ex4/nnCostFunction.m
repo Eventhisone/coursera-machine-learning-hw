46f5166ff359ff19beb3486cc02ea040aaed2e04
Zach Turner <turnerzdp@gmail.com>
2018-08-20 12:53:47 -0600

    Finished ex4. Edited nnCostFunction, sigmoidGradient, and randInitializeWeights.

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -38,7 +38,30 @@ Theta2_grad = zeros(size(Theta2));
 %         variable J. After implementing Part 1, you can verify that your
 %         cost function computation is correct by verifying the cost
 %         computed in ex4.m
-%
+
+szx = size(X);
+% sz2 = size(Theta2);
+
+% Expand y into a matrix of single values
+eye_matrix = eye(num_labels);
+y_matrix = eye_matrix(y,:);
+
+% Perform forward propagation
+a1 = [ones(szx(1), 1) X];
+z2 = a1 * Theta1';
+a2 = sigmoid(z2);
+sz2 = size(a2);
+a2 = [ones(sz2(1), 1) a2];
+z3 = a2 * Theta2';
+a3 = sigmoid(z3);
+
+% Compute the double sum to get J
+term1 = -y_matrix .* log(a3);
+term2 = (1 - y_matrix) .* log(1 - a3);
+% J = (1 / m) * sum(sum(term1 - term2));
+regTerm = (lambda / (2 * m)) * (sum(sum(Theta1(:, 2:end) .* Theta1(:, 2:end))) + sum(sum(Theta2(:, 2:end) .* Theta2(:, 2:end))));
+J = ((1 / m) * sum(sum(term1 - term2))) + regTerm;
+
 % Part 2: Implement the backpropagation algorithm to compute the gradients
 %         Theta1_grad and Theta2_grad. You should return the partial derivatives of
 %         the cost function with respect to Theta1 and Theta2 in Theta1_grad and
@@ -54,6 +77,17 @@ Theta2_grad = zeros(size(Theta2));
 %               over the training examples if you are implementing it for the 
 %               first time.
 %
+
+% Vectorized implementation
+delta3 = a3 - y_matrix;
+delta2 = delta3 * Theta2(:, 2:end) .* sigmoidGradient(z2);
+
+Delta1 = delta2' * a1;
+Delta2 = delta3' * a2;
+
+Theta1_grad = (1 / m) * Delta1;
+Theta2_grad = (1 / m) * Delta2;
+
 % Part 3: Implement regularization with the cost function and gradients.
 %
 %         Hint: You can implement this around the code for
@@ -62,21 +96,14 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+% Set the first row of Theta1 and Theta2 to 0
+Theta1(:, 1) = 0;
+Theta2(:, 1) = 0;
 
+Theta1_grad = Theta1_grad + ((lambda / m) * Theta1);
+Theta2_grad = Theta2_grad + ((lambda / m) * Theta2);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+grad = [Theta1_grad(:) ; Theta2_grad(:)];
 
 
 
